6031d6dd670b545943b880755947506ab92d5d86
panhiathao <panhiathao@siliconwat.com>
2020-07-08 17:41:39 -0700

    content ideas

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,16 @@
 function hello() {
     alert("hi I am Pa Nhia")
 }
+
+window.onscroll = barSticky()
+const navbar = document.getElementById("navbar")
+
+const sticky = navbar.offsetTop;
+
+function barSticky() {
+    if(window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+    }else {
+        navbar.classList.remove("sticky")
+    }
+}
