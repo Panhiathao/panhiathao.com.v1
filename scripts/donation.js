b2785cca38734aa1f8b7cd6c8ff55df99f590f10
panhiathao <panhiathao@siliconwat.com>
2020-07-15 18:32:08 -0700

    add 2 button for donation page and update css pages

--- scripts/donation.js
+++ scripts/donation.js
@@ -26,3 +26,65 @@
    });
  });
 })();
+
+
+(function() {
+  var stripe = Stripe('pk_live_jpuOs5XhYCq9ua2F8MbIa1so');
+
+  var checkoutButton = document.getElementById('checkout-button-price_1H52LSHkRWTAGSa4iJPlS8lT');
+  checkoutButton.addEventListener('click', function () {
+    // When the customer clicks on the button, redirect
+    // them to Checkout.
+    stripe.redirectToCheckout({
+      lineItems: [{price: 'price_1H52LSHkRWTAGSa4iJPlS8lT', quantity: 1}],
+      mode: 'subscription',
+      // Do not rely on the redirect to the successUrl for fulfilling
+      // purchases, customers may not always reach the success_url after
+      // a successful payment.
+      // Instead use one of the strategies described in
+      // https://stripe.com/docs/payments/checkout/fulfillment
+      successUrl: 'https://siliconwat.com/pages/thankyou.html',
+      cancelUrl: 'https://siliconwat.com/pages/donation.html',
+    })
+    .then(function (result) {
+      if (result.error) {
+        // If `redirectToCheckout` fails due to a browser or network
+        // error, display the localized error message to your customer.
+        var displayError = document.getElementById('error-message');
+        displayError.textContent = result.error.message;
+      }
+    });
+  });
+})();
+
+
+(function() {
+  var stripe = Stripe('pk_live_jpuOs5XhYCq9ua2F8MbIa1so');
+
+  var checkoutButton = document.getElementById('checkout-button-price_1H5KccHkRWTAGSa4E9eKYNrU');
+  checkoutButton.addEventListener('click', function () {
+    // When the customer clicks on the button, redirect
+    // them to Checkout.
+    stripe.redirectToCheckout({
+      lineItems: [{price: 'price_1H5KccHkRWTAGSa4E9eKYNrU', quantity: 1}],
+      mode: 'payment',
+      // Do not rely on the redirect to the successUrl for fulfilling
+      // purchases, customers may not always reach the success_url after
+      // a successful payment.
+      // Instead use one of the strategies described in
+      // https://stripe.com/docs/payments/checkout/fulfillment
+      successUrl: 'https://siliconwat.com/pages/thankyou.html',
+      cancelUrl: 'https://siliconwat.com/pages/donation.html',
+    })
+    .then(function (result) {
+      if (result.error) {
+        // If `redirectToCheckout` fails due to a browser or network
+        // error, display the localized error message to your customer.
+        var displayError = document.getElementById('error-message');
+        displayError.textContent = result.error.message;
+      }
+    });
+  });
+})();
+
+
