0776ff5b9cd324a7ed5be524b75b7f6ebf1f5464
panhiathao <panhiathao@siliconwat.com>
2020-08-28 16:52:45 -0700

    add custom tag to html files

--- scripts/cancel.js
+++ scripts/cancel.js
@@ -1,2 +1,5 @@
-import {PtFooter} from "..scripts/components/pt-footer.mjs"
+import {PtFooter} from "./components/pt-footer.mjs"
 customElements.define("pt-footer", PtFooter)
+
+import {PtNav} from "./components/pt-nav.mjs"
+customElements.define("pt-nav", PtNav)
