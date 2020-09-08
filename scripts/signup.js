4789049c2aeb9e5ceadf52d95bf4ff2ec74a39d3
panhiathao <panhiathao@siliconwat.com>
2020-09-08 14:34:48 -0700

    update signup event

--- scripts/signup.js
+++ scripts/signup.js
@@ -1,6 +1,11 @@
 import {HbSignup} from "https://library.siliconwat.com/components/hb-signup.mjs";
 customElements.define("hb-signup", HbSignup);
 
+const signup = document.querySelector("hb-signup");
+signup.addEventListener("submit", () => document.querySelector("img").src = "https://blog.eshop-rychle.cz/wp-content/uploads/2016/11/loading-bar-1.gif")
+signup.addEventListener("success", () => document.querySelector("img").src = "https://blog.eshop-rychle.cz/wp-content/uploads/2016/11/loading-bar-1.gif")
+signup.addEventListener("done", () => document.querySelector("img").src = "https://blog.eshop-rychle.cz/wp-content/uploads/2016/11/loading-bar-1.gif")
+
 
 var firebaseConfig = {
     apiKey: "AIzaSyBKejGRdXVpLJqwWI0aP4HYHfwsceFfARE",
