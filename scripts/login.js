2b3e7f67a567f46bcc819cb250b460940c1cd748
panhiathao <panhiathao@siliconwat.com>
2020-09-12 15:40:14 -0700

    correction on login html

--- scripts/login.js
+++ scripts/login.js
@@ -12,11 +12,12 @@ var firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 
-import {HbLogin} from "https://library.siliconwat.com/components/hb-login.mjs";
-customElements.define("hb-login", HbLogin);
+import {HbLogin} from "https://library.siliconwat.com/components/hb-login.mjs"
+customElements.define("hb-login", HbLogin)
 
 const img = document.querySelector("img")
 const login = document.querySelector("hb-login")
+
 login.addEventListener("submit", () => {
   img.src = "https://lh3.googleusercontent.com/eqE94HW5lLt3_yBZHnEtLTMtbD3bfh6UAgSWEdQcNByfuEEXrrJgUrAUjfZ_9Du_31K4=s162"
   console.log("spin in")
@@ -29,8 +30,10 @@ login.addEventListener("done", () => {
 
  login.addEventListener("click", () => {
   img.src = "https://lh3.googleusercontent.com/eqE94HW5lLt3_yBZHnEtLTMtbD3bfh6UAgSWEdQcNByfuEEXrrJgUrAUjfZ_9Du_31K4=s162"
-   console.log("spin in")
+  console.log("spin inner")
  })
+
+
 
 
 
