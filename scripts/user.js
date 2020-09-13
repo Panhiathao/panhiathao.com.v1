bc61ad3519f501ccec74dfdd48234d627546b578
panhia thao <panhiathao@siliconwat.com>
2020-09-12 20:21:09 -0700

    add button css for components

--- scripts/user.js
+++ scripts/user.js
@@ -1,21 +1,21 @@
+
+
 var firebaseConfig = {
-  apiKey: "AIzaSyBKejGRdXVpLJqwWI0aP4HYHfwsceFfARE",
-  authDomain: "panhia-2040e.firebaseapp.com",
-  databaseURL: "https://panhia-2040e.firebaseio.com",
-  projectId: "panhia-2040e",
-  storageBucket: "panhia-2040e.appspot.com",
-  messagingSenderId: "1069882940906",
-  appId: "1:1069882940906:web:a81e965ffada6e4a8c2693",
-  measurementId: "G-VS2DWZ35TZ"
-};
+    apiKey: "AIzaSyBKejGRdXVpLJqwWI0aP4HYHfwsceFfARE",
+    authDomain: "panhia-2040e.firebaseapp.com",
+    databaseURL: "https://panhia-2040e.firebaseio.com",
+    projectId: "panhia-2040e",
+    storageBucket: "panhia-2040e.appspot.com",
+    messagingSenderId: "1069882940906",
+    appId: "1:1069882940906:web:a81e965ffada6e4a8c2693",
+    measurementId: "G-VS2DWZ35TZ"
+  };
 
-firebase.initializeApp(firebaseConfig);
-
-
-import {HbAccountVerify} from "https://library.siliconwat.com/components/hb-account.mjs";
+  firebase.initializeApp(firebaseConfig);
+import {HbAccountVerify} from "https://library.siliconwat.com/components/hb-account.mjs"
     customElements.define("hb-account-verify", HbAccountVerify)
 
-import {HbAccountEmail} from "https://library.siliconwat.com/components/hb-account.mjs";
+import {HbAccountEmail} from "https://library.siliconwat.com/components/hb-account.mjs"
     customElements.define("hb-account-email", HbAccountEmail)
 
 const img = document.querySelector("img");
@@ -34,11 +34,12 @@ const accountEmail = document.querySelector("hb-account-email")
 
 
     
-    firebase.auth().onAuthStateChanged(user => {
-      if (user) {
-        console.log(user.uid, user.email)
-      } else {
-        console.log("not logged in")
-      }
-    })
     
+    firebase.auth().onAuthStateChanged(user => {
+    if (user) {
+      console.log(user.uid, user.email)
+    } else {
+      console.log("not logged in")
+    }
+  })
+  
